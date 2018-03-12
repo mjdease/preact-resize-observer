a54d43af0803a4ffb70a07d0447049f0809ef0a7
mjdease <mjdease@users.noreply.github.com>
2018-03-11 23:39:52 -0300

    Add basic js example project

---
+++ examples/basic/index.js
@@ -0,0 +1,48 @@
+import { Component, options } from 'preact';
+import PropTypes from 'prop-types';
+import ResizeObserver from 'preact-resize-observer/dev';
+
+import './style';
+
+export default class App extends Component {
+  /* eslint-disable-next-line react/sort-comp */
+  constructor() {
+    super();
+
+    this.state = {
+      width: 0,
+      height: 0
+    };
+  }
+
+  handleResize = (width, height) => {
+    this.setState({
+      width,
+      height
+    });
+  }
+
+  render() {
+    return (
+      <div id="app">
+        <div className="stats">
+          <h2>{this.state.width} x {this.state.height}</h2>
+        </div>
+        <ResizeObserver onResize={this.handleResize} />
+      </div>
+    );
+  }
+}
+
+// Check prop-types when nodes are updated
+const oldVnodeHook = options.vnode;
+options.vnode = (vnode) => {
+  const tag = vnode.nodeName;
+  if (typeof tag === 'function' && tag.propTypes) {
+    const componentName = tag.displayName || tag.name;
+    PropTypes.checkPropTypes(tag.propTypes, vnode.attributes, 'prop', componentName);
+  }
+  if (oldVnodeHook) {
+    oldVnodeHook(vnode);
+  }
+};
