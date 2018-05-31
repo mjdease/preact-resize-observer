f9a204c3722b9752c1ed7258d3c58eefb9be96e7
mjdease <mjdease@users.noreply.github.com>
2018-05-31 02:00:19 -0300

    Switch to Yarn due to npm#20835 Change examples to use the package as a linked dependency. Change examples to only resolve modules directly in /node_modules Add support to the typescript example to be run locally and on github.

--- examples/basic/preact.config.js
+++ examples/basic/preact.config.js
@@ -1,4 +1,3 @@
-import preactCliTypeScript from 'preact-cli-plugin-typescript';
 import path from 'path';
 
 /**
@@ -10,11 +9,6 @@ import path from 'path';
  * @param {WebpackConfigHelpers} helpers object with useful helpers when working with config.
  **/
 export default function (config, env, helpers) {
-  preactCliTypeScript(config);
-  // `npm run build-demo` outputs to a github hosted directory
-  if (process.env.NODE_ENV === 'production') {
-    config.output.publicPath = '/preact-resize-observer/demo/';
-  }
   // Only use modules directly in /node_modules, ensures parent devDependencies aren't used
   config.resolve.modules = [path.resolve(__dirname, 'node_modules')];
 }
